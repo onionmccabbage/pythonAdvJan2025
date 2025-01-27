e270fb45f1edc1455290d9190c1520efe1a4da8e
onionmccabbage <onionmccabbage@live.com>
2025-01-27 11:28:59 +0000

    operators we may override

--- some_classes/word_class.py
+++ some_classes/word_class.py
@@ -8,6 +8,20 @@ class Word(): # here we choose to inherit from object (implicitly)
         '''We override the built-in equlaity operator''' 
         return str(self.word).lower() == str(otherWord.word).lower()       
 
+# __ne__ not equal
+# __gt__ greater than
+# __lt__ less than
+# __ge__ and __le__ greater-or-equal and less-or-equal
+
+# __add__( self, other ) self + other
+# __sub__( self, other ) self - other
+# __mul__( self, other ) self * other
+# __floordiv__( self, other ) self // other
+# __truediv__( self, other ) self / other
+# __mod__( self, other ) self % other
+# __pow__( self, other ) self ** other
+# __len__ is the length of the object
+
 if __name__ == '__main__':
     # exercise our code
     w1 = 'hello'
