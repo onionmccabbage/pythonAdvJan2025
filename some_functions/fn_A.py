3acb6c852b7c947ed5fcd4e13d3d1c6fd8a2286e
onionmccabbage <onionmccabbage@live.com>
2025-01-27 12:29:03 +0000

    using args and kwargs

--- some_functions/fn_A.py
+++ some_functions/fn_A.py
@@ -19,14 +19,25 @@ def usingKWArgs(**kwargs): # the double asterisk gathers all keyword arguments i
 def both(*args, **kwargs): # any keyword arguments must come AFTER any positional arguments
     print(args, kwargs)
 
-
-
-
-
-
+# a practical use of this
+def actDependantly(*args):
+    '''this function acts differently according to the quantity of positional arguments'''
+    if len(args)==0:
+        return 'no args'
+    elif len(args)==1:
+        return args[0]
+    elif len(args)==2:
+        return args[0] + args[1]
+    else:
+        return args[len(args)-1] # return the last argument
+    
 if __name__ == '__main__':
     # call our functions
     # here we have arguments which are positional: 0, 1, 2, 3....
     usingArgs(4, 5, 6, True, [], (3,2,1), 'done')
     usingKWArgs(x=3, y=4, v=True, c=[6,5,4])
     both(4,3,2, x=99)
+    print(actDependantly())
+    print(actDependantly(1))
+    print(actDependantly(1,2))
+    print(actDependantly(1,2,3,4,5))
