bbea6297ec33759c191a9c1f32b8c65c04342a96
onionmccabbage <onionmccabbage@live.com>
2025-01-27 10:30:14 +0000

    using slots

--- some_classes/my_shape.py
+++ some_classes/my_shape.py
@@ -2,21 +2,33 @@ from basic_classes import AbstractShape
 
 class Shape(AbstractShape):
     '''This shape will have a number of sides'''
-    def __init__(self, num_sides):
+    __slots__ = ('__num_sides','__colour') # a tuple of permitted properties of this class
+    def __init__(self, num_sides, colour):
         '''This method is called every time we create an instance'''
-        self.num_sides = num_sides
+        self.num_sides = num_sides # here we are calling the setter methods of this class
+        self.colour    = colour
     # we may declare property getters and setters
     @property
     def num_sides(self):
+        # we use name mangling to prevent direct access to properties of this class
         return self.__num_sides
     @num_sides.setter
     def num_sides(self, num_sides):
+        # carry out validation
         self.__num_sides = num_sides
     # we must implement a __str__ method
+    @property
+    def colour(self):
+        return self.__colour
+    @colour.setter
+    def colour(self, colour):
+        self.__colour = colour
     def __str__(self):
-        return f'this is my shape with {self.__num_sides} sides'
+        return f'this is my shape with {self.num_sides} sides, colour is {self.colour}'
 
 if __name__ == '__main__':
     # make an instance of our class
-    triangle = Shape(3)
+    triangle = Shape(3, 'blue')
+    # attempt to add arbitrary property to our class
+    triangle.__best_before = 9 # this is an arbitrary property....
     print(triangle)
