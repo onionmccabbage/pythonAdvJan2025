cb01faee8977f500dd8c6c4dd1876001302ee5f2
onionmccabbage <onionmccabbage@live.com>
2025-01-27 11:58:22 +0000

    class methods and properties

--- some_classes/my_figure.py
+++ some_classes/my_figure.py
@@ -1,9 +1,16 @@
 from my_shape import Shape
 
 class Figure(Shape): # we now have all the fatures of the Shape class
+    # we may give properties to the class itself (not to any particluar instance)
+    count = 0
     def __init__(self, num_sides, colour, id):
         super().__init__(num_sides, colour) # call the init of the parent class
         self.id = id
+        Figure.count += 1 # increase the count every t ime we make a new instance
+    @classmethod
+    def howMany(cls): 
+        '''This method belongs to the class (not to any particular instance)'''
+        return f'There are {cls.count} instances of this class'
     @property
     def id(self):
         return self.__id
@@ -19,5 +26,9 @@ class Figure(Shape): # we now have all the fatures of the Shape class
 
 if __name__ == '__main__':
     fig1 = Figure(5, 'pink', 44)
+    fig2 = Figure(2, 'grey', 45)
+    fig3 = Figure(6, 'yellow', 46)
+    fig4 = Figure(3, 'vermillion', 47)
     print(fig1)
+    print(Figure.howMany())
 
