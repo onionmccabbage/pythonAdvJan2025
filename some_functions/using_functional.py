368f418b28053201f924acc58a97ea2ed55d95b3
onionmccabbage <onionmccabbage@live.com>
2025-01-27 15:39:53 +0000

    using map and filter

---
+++ some_functions/using_functional.py
@@ -0,0 +1,20 @@
+# as well as class-based progams, we may write function-based programs
+# map lets us apply a function acroos a collection of values
+from util import squareIt
+from util import isOdd
+
+if __name__ == '__main__':
+    # we need some data 
+    d = (4,6,2,8,9,2,6)
+    e = [9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4]
+    # using map
+    squares = map(squareIt, d)
+    print(squares)
+    # we then iterate over the map object to see any results
+    print( squares.__next__() ) # 16
+    print('..............')
+    for r in squares:
+        print(r)
+    # using filter
+    odds = filter(isOdd, e)
+    print(odds)
---
+++ some_functions/util.py
@@ -0,0 +1,7 @@
+def squareIt(a):
+    '''return the square of a'''
+    return a*a
+
+def isOdd(w):
+    '''return True if it is odd, Fasle otherwise'''
+    return w%2 !=0
