cc34f748e41ce1ed65ceb18224f96e90b1ca1fc1
onionmccabbage <onionmccabbage@live.com>
2025-01-28 10:53:31 +0000

    insert data into DB

---
+++ db/write_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def writeDB():
+    '''write a single entry to the new database table'''
+    conn = sqlite3.connect('my_db') # this code is common across many modules
+    curs = conn.cursor()
+    # SQL statement
+    st = '''
+    INSERT INTO zoo
+    VALUES ("Penguin", 16, 0.52)
+''' 
+    try:
+        curs.execute(st)
+        conn.commit()
+        conn.close()
+    except Exception as err:
+        print(f'{err}')
+
+if __name__ == '__main__':
+    writeDB()
