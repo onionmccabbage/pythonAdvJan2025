51f96322dbbd7b45519a722a6ec87faddccdb02b
onionmccabbage <onionmccabbage@live.com>
2025-01-28 10:23:48 +0000

    makiing calls with parameters

--- using_API/api_access.py
+++ using_API/api_access.py
@@ -1,17 +1,18 @@
 # NB the requests library wraps the older 'urllib3' library that is part of python
 import requests
+from random import randint
 
 # remember - it is often a good idea to avoid putting anything in the global namespace
-apiURL = 'https://jsonplaceholder.typicode.com/users'
+apiURL = 'https://nonsense.typicode.com/users'
 
 
-def getUsers():
+def getUsers(n=1): # we should think about validating n
     '''fetch some user data from an API, allow the possibility of parameters'''
     global apiURL # we now have direct access to the apiUrl that was defined globally within this module
     # whenever we are dealing with i/o bound data, its a good idea to handle exceptions
     try:
-        response = requests.get(apiURL) # here we make a request to the URL
-        print(response.status_code)
+        response = requests.get(f'{apiURL}/{n}') # here we make a request to the URL
+        print(response.status_code) # 200 is good news, 404 means not found (but is not an error)
         print(response.raw) # or .text or .json or .html .xml etc.
         # grab the JSON from the response
         res_j = response.json() # the JSON text will be converted to a Python structure
@@ -21,11 +22,18 @@ def getUsers():
         print(f'There was a problem: {err}')
     finally:
         '''always runs, even if there is an exception'''
+        # typically we write tidy-up code in the finally block
+        print('all done')
+    print('this always runs!! (unless there is a terminal exception)')
 
+def randNum():
+    '''return a random integer 1-10'''
+    return randint(1,10)
 
 def main():
     '''exercise the code'''
-    r = getUsers()
+    n = randNum()
+    r = getUsers(n)
     print(r, type(r))
 
 if __name__ == '__main__':
