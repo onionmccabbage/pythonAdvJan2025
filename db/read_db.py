25a253e774c8ab9cd7da5aa3e023ad4fcd75ad66
onionmccabbage <onionmccabbage@live.com>
2025-01-28 11:57:33 +0000

    retrieve everything from DB table

---
+++ db/read_db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def readDB():
+    ''' read back all the data from the DB table'''
+    conn = sqlite3.connect('my_db')
+    curs = conn.cursor()
+    # SELECT * is permissible but we tend to use explicit field names
+    # the terminating ; ends the SQL statement (reduces the risk of injection attacks)
+    st = '''
+    SELECT creature, count, cost FROM zoo;
+'''
+    try:
+        curs.execute(st)
+        conn.commit()
+        # we may now read back from the cursor
+        rows = curs.fetchall()
+        conn.close()
+        return rows
+    except Exception as err: # we may have multiple exception handlers, each for a possible exception type
+        print(err)
+
+if __name__ == '__main__':
+    r = readDB()
+    print(r, type(r))
