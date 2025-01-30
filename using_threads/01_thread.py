89fd9ddb4cd17e22eb5227dfba50c7824247cd21
onionmccabbage <onionmccabbage@live.com>
2025-01-30 10:14:21 +0000

    start and join threads

--- using_threads/01_thread.py
+++ using_threads/01_thread.py
@@ -4,7 +4,7 @@ import time
 def fn(n):
     '''this is a normal function just like any other'''
     time.sleep( 1 )
-    print(f'This is fnction {n}')
+    print(f'This is function {n}')
 
 if __name__ == '__main__':
     #we could invoke the function procedurally (in the normal way)
@@ -17,7 +17,12 @@ if __name__ == '__main__':
     tB = Thread(target=fn, args=('B',))
     tC = Thread(target=fn, args=('C',))
     print('on the main thread')
-    tA.start()
+    tA.start() # the threads will begin in the order they are started
     tB.start()
     tC.start()
+    # by default, running additional threads does not block the main thread
+    # it is common to use 'join' so tje main thread waits for the other threads to complete
+    tB.join()
+    tA.join()
+    tC.join()
     print('all done')
