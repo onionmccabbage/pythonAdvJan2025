f555c10a555cfe20589ba6b5e0cca5ea8a3135bb
onionmccabbage <onionmccabbage@live.com>
2025-01-30 10:05:17 +0000

    simple function threads

---
+++ using_threads/01_thread.py
@@ -0,0 +1,23 @@
+from threading import Thread
+import time
+
+def fn(n):
+    '''this is a normal function just like any other'''
+    time.sleep( 1 )
+    print(f'This is fnction {n}')
+
+if __name__ == '__main__':
+    #we could invoke the function procedurally (in the normal way)
+    # fn('A')
+    # fn('B')
+    # fn('C')
+    # we try to control threads through the Thread class
+    # threads are ultimately managesby the operating system
+    tA = Thread(target=fn, args=('A',)) # a one-member tuple
+    tB = Thread(target=fn, args=('B',))
+    tC = Thread(target=fn, args=('C',))
+    print('on the main thread')
+    tA.start()
+    tB.start()
+    tC.start()
+    print('all done')
